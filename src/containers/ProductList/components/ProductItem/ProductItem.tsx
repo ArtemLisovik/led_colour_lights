98e89b56f51d49287a326d7000cef1faf6e489ee
Artem Lisovik <107492333+ArtemLisovik@users.noreply.github.com>
2023-08-29 16:07:01 +0300

    colors & length added

--- src/containers/ProductList/components/ProductItem/ProductItem.tsx
+++ src/containers/ProductList/components/ProductItem/ProductItem.tsx
@@ -3,18 +3,24 @@ import './ProductItem.scss'
 import { useDispatch } from 'react-redux'
 import { addProduct } from 'containers/Cart/store/CartSlice'
 import { Link, useParams } from 'react-router-dom'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Zoom, toast } from 'react-toastify'
 
 export const ProductItem = ({ colors, name, newPrice, description, image, oldPrice, id, options, length }: ProductType) => {
 
   const [selectedLength, setSelectedOption] = useState()
-  const [selectedColor, setSelectedColor] = useState()
+  const [selectedColor, setSelectedColor] = useState<any>()
+
+  useEffect(() => {
+    if (colors) {
+      setSelectedColor(Object.entries(colors)[0])
+    }
+    
+  }, [colors])
 
   const dispatch = useDispatch()
 
-  const category = useParams().category
-  const sub = useParams().subcategory
+
 
   const optionHandler = (e: any, option: any) => {
     e.preventDefault()
@@ -85,44 +91,63 @@ export const ProductItem = ({ colors, name, newPrice, description, image, oldPri
       });
     }
   }
+
+  // if (colors) {
+  //   Object.entries(colors).map((color:any) => {
+  //     console.log(color)
+  //   })
+  // }
   return (
     <Link to={`${id}`} className="new__card card">
-      <img loading='lazy' src={image} alt="product image" className="new__card__photo card__photo" />
-
-      <ul className="card__colors-list">
-        {colors && colors.map(color => {
-          return (
-            <li
-              onClick={(e) => colorHandler(color, e)}
-              style={{ background: `${color}`, boxShadow: `${selectedColor === color ? `0 0 10px ${color} ` : 'none'}` }}
-              key={color}
-              className={selectedColor === color ? `card__colors-item active` : `card__colors-item`}
-            >
-            </li>
-          )
-        })}
-      </ul>
+      <img loading='lazy' src={selectedColor?.[1]} alt="product image" className="new__card__photo card__photo" />
 
 
       <h3 className="new__card__title card__title">{name}</h3>
       <p className="new__card__descr card__descr">{description}</p>
-      <ul className="card__extra-options">
 
-        {
-          length && length.map(lengthItem => {
-            return (
-              <li
-                onClick={(e) => optionHandler(e as any, lengthItem)}
-                key={lengthItem}
-                className={selectedLength === lengthItem ? `card__extra-options-item selected` : `card__extra-options-item`}
-              >
-                {lengthItem} м
-              </li>
-            )
-          })
-        }
 
-      </ul>
+      {
+        <>
+          <p className="card__extra-options-title">Колір:</p>
+          <ul className="card__colors-list">       
+            
+            {colors && Object.entries(colors).map(color => {
+              console.log(color[0] === selectedColor?.[0])
+              return (
+                <li
+                  onClick={(e) => colorHandler(color, e)}
+                  key={color as any}
+                  className={selectedColor?.[0] === color[0] ? `card__colors-item active` : `card__colors-item`}
+                >
+                  {color[0]}
+                </li>
+              )
+
+            })}
+          </ul>
+        </>
+      }
+
+      {
+        <>
+          <p className="card__extra-options-title">Довжина:</p>
+          <ul className="card__extra-options">
+            {length && length.map(lengthItem => {
+              return (
+                <li
+                  onClick={(e) => optionHandler(e as any, lengthItem)}
+                  key={lengthItem}
+                  className={selectedLength === lengthItem ? `card__extra-options-item selected` : `card__extra-options-item`}
+                >
+                  {lengthItem} м
+                </li>
+              )
+            })}
+          </ul>
+        </>
+      }
+
+
       <p className="new__card__price card__price">{newPrice} грн {oldPrice && <span className='card__old-price'>{oldPrice} грн</span>}</p>
       <button
         onClick={(e) => buyProduct(e)}
